1fc2f9aa2f8b2443f002d5812cb94bc3c923968c
razor54 <andregaudencio5@gmail.com>
2017-10-20 21:41:31 +0100

    some updates but eventualy reafactor

--- Pairing/Pairing.cs
+++ Pairing/Pairing.cs
@@ -16,31 +16,42 @@ namespace Pairing
         private T _elemt1;
         private U _element2;
 
-        
 
         // throws ThreadInterruptedException, TimeoutException
         public Tuple<T, U> Provide(T value, int timeout)
         {
             Monitor.Enter(_monitor);
             LinkedListNode<T> node = _tList.AddLast(value);
+
+
             Tuple<T, U> toRet = null;
             try
             {
-                
-                if (node == _tList.First && _uList.Any())
-                {
-                    LinkedListNode<U> uListFirst = _uList.First;
+                LinkedListNode<U> uListFirst;
 
+                if (node == _tList.First)
+                {
                     _elemt1 = node.Value;
 
-                    Monitor.Enter(uListFirst);
-                    Monitor.Pulse(uListFirst);
-                    Monitor.Exit(uListFirst);
+                    
 
-                    Monitor.Enter(node);
+                    Monitor.Enter(_tList);
+                    Monitor.Pulse(_tList);
+                    Monitor.Exit(_tList);
 
-                    Monitor.Exit(_monitor);
-                    bool isNotTimeout = Monitor.Wait(node, timeout);
+                    if ((uListFirst = _uList.First) != null)
+                    {
+                        Monitor.Enter(uListFirst);
+                        Monitor.Pulse(uListFirst);
+                        Monitor.Exit(uListFirst);
+                    }
+                     Monitor.Exit(_monitor);
+                    Monitor.Enter(_uList);
+                   
+
+                    bool isNotTimeout = Monitor.Wait(_uList, timeout);
+
+                    Monitor.Exit(_uList);
                     Monitor.Enter(_monitor);
 
                     if (!isNotTimeout)
@@ -55,19 +66,55 @@ namespace Pairing
                     //verify if elems are null or not
                     //if they are it means there was a timeout or exception
                     //???
-                    if (_elemt1.Equals(default(T)) || _element2.Equals(default(U)))
+                    if (_elemt1 == null || _elemt1.Equals(default(T)) || _element2 == null ||
+                        _element2.Equals(default(U)))
                     {
                         toRet = null;
                         //threre was something wrong so return null
                         return null;
                     }
                     //retirar os valores para a proxima iteraçao
+                    Console.WriteLine("SAIU PPOR PROVIDE T -- 1");
                     toRet = new Tuple<T, U>(_elemt1, _element2);
                     return toRet;
                 }
+                Monitor.Exit(_monitor);
 
 
+                Monitor.Enter(_uList);
 
+                bool isnotTimeout = Monitor.Wait(_uList, timeout);
+                Monitor.Exit(_uList);
+
+                Monitor.Enter(node);
+                Monitor.Wait(node, timeout);
+                Monitor.Exit(node);
+                Monitor.Enter(_monitor);
+
+                if (!isnotTimeout)
+                {
+                    //tirar os pairings
+                    _elemt1 = default(T);
+                    _element2 = default(U);
+
+                    return null;
+                }
+                Monitor.Exit(_monitor);
+
+                Monitor.Enter(_tList);
+                Monitor.Pulse(_tList);
+                Monitor.Exit(_tList);
+
+                uListFirst = _uList.First;
+
+                Monitor.Enter(uListFirst);
+                Monitor.Pulse(uListFirst);
+                Monitor.Exit(uListFirst);
+
+                Monitor.Enter(_monitor);
+                Console.WriteLine("SAIU PPOR PROVIDE T---2");
+                return new Tuple<T, U>(_elemt1, _element2);
+                /*
                 Monitor.Enter(node);
 
                 Monitor.Exit(_monitor);
@@ -92,13 +139,16 @@ namespace Pairing
                     //threre was something wrong so return null
                     return null;
                 }
+                uListFirst = _uList.First;
                 //retirar os valores para a proxima iteraçao
+                Monitor.Enter(uListFirst);
+                Monitor.Pulse(uListFirst);
+                Monitor.Exit(uListFirst);
                 toRet = new Tuple<T, U>(_elemt1, _element2);
                 return toRet;
-
-
+                */
             }
-            
+
             catch (ThreadInterruptedException)
             {
                 _elemt1 = default(T);
@@ -108,8 +158,12 @@ namespace Pairing
             finally
             {
                 _tList.Remove(node);
-                if(toRet!=null)
-                     Monitor.Exit(node);
+                if (_tList.Any())
+                {
+                    Monitor.Enter(_tList.First);
+                    Monitor.Pulse(_tList.First);
+                    Monitor.Exit(_tList.First);
+                }
                 Monitor.Exit(_monitor);
             }
         }
@@ -129,74 +183,33 @@ namespace Pairing
             LinkedListNode<U> node = _uList.AddLast(value);
 
             Tuple<T, U> toRet = null;
+
             try
             {
-                if (node == _uList.First && _tList.Any())
-                {
-                    LinkedListNode<T> tListFirst = _tList.First;
-                    _element2 = node.Value;
-
-                    Monitor.Enter(tListFirst);
-                    Monitor.Pulse(tListFirst);
-                    Monitor.Exit(tListFirst);
-
-                    Monitor.Enter(node);
-
-                    Monitor.Exit(_monitor);
-                    bool isNotTimeout = Monitor.Wait(node, timeout);
-                    Monitor.Enter(_monitor);
-
-                    if (!isNotTimeout)
-                    {
-                        //tirar os pairings
-                        _elemt1 = default(T);
-                        _element2 = default(U);
-                        toRet = null;
-                        return null;
-                    }
-
-                    //verify if elems are null or not
-                    //if they are it means there was a timeout or exception
-                    //???
-                    if (_elemt1.Equals(default(T)) || _element2.Equals(default(U)))
-                    {
-                        //threre was something wrong so return null
-                        toRet = null;
-                        return null;
-                    }
-                    toRet = new Tuple<T, U>(_elemt1, _element2); ;
-                    //retirar os valores para a proxima iteraçao
-                    return toRet;
-                }
-                Monitor.Exit(_monitor);
-
-                
-                while (!_tList.Any())
-                {
-                    if (Environment.TickCount > targetTime)
-                        return null;
-                }
-
-                Monitor.Enter(_monitor);
-
-                var first = _tList.First;
-                Monitor.Enter(first);
-                Monitor.Pulse(first);
-                Monitor.Exit(first);
-
+                LinkedListNode<T> tListFirst ;
                 if (node == _uList.First)
                 {
-                    LinkedListNode<T> tListFirst = _tList.First;
                     _element2 = node.Value;
 
-                    Monitor.Enter(tListFirst);
-                    Monitor.Pulse(tListFirst);
-                    Monitor.Exit(tListFirst);
+                   
 
-                    Monitor.Enter(node);
+                    Monitor.Enter(_uList);
+                    Monitor.Pulse(_uList);
+                    Monitor.Exit(_uList);
+
+                    if ((tListFirst = _tList.First) != null)
+                    {
+                        Monitor.Enter(tListFirst);
+                        Monitor.Pulse(tListFirst);
+                        Monitor.Exit(tListFirst);
+                    }
 
                     Monitor.Exit(_monitor);
-                    bool isNotTimeout = Monitor.Wait(node, timeout);
+                    Monitor.Enter(_tList);
+
+
+                    bool isNotTimeout = Monitor.Wait(_tList, timeout);
+                    Monitor.Exit(_tList);
                     Monitor.Enter(_monitor);
 
                     if (!isNotTimeout)
@@ -211,23 +224,59 @@ namespace Pairing
                     //verify if elems are null or not
                     //if they are it means there was a timeout or exception
                     //???
-                    if (_elemt1.Equals(default(T)) || _element2.Equals(default(U)))
+                    if (_elemt1 == null || _elemt1.Equals(default(T)) || _element2 == null ||
+                        _element2.Equals(default(U)))
                     {
                         //threre was something wrong so return null
                         toRet = null;
                         return null;
                     }
-                    toRet = new Tuple<T, U>(_elemt1, _element2); ;
+                    toRet = new Tuple<T, U>(_elemt1, _element2);
+                    ;
                     //retirar os valores para a proxima iteraçao
+                    Console.WriteLine("SAIU PPOR PROVIDE U---1");
                     return toRet;
                 }
 
-                return null;
+                Monitor.Exit(_monitor);
 
+
+                Monitor.Enter(_tList);
+
+                bool isnotTimeout = Monitor.Wait(_tList, timeout);
+                Monitor.Exit(_tList);
+
+                Monitor.Enter(node);
+                Monitor.Wait(node, timeout);
+                Monitor.Exit(node);
+
+                Monitor.Enter(_monitor);
+                if (!isnotTimeout)
+                {
+                    //tirar os pairings
+                    _elemt1 = default(T);
+                    _element2 = default(U);
+
+                    return null;
+                }
+                tListFirst = _tList.First;
+                Monitor.Exit(_monitor);
+
+
+                Monitor.Enter(_uList);
+                Monitor.Pulse(_uList);
+                Monitor.Exit(_uList);
+
+                Monitor.Enter(tListFirst);
+                Monitor.Pulse(tListFirst);
+                Monitor.Exit(tListFirst);
+
+                Monitor.Enter(_monitor);
+                Console.WriteLine("SAIU PPOR PROVIDE U---2");
+                return new Tuple<T, U>(_elemt1, _element2);
             }
             catch (ThreadInterruptedException)
             {
-
                 _elemt1 = default(T);
                 _element2 = default(U);
 
@@ -236,12 +285,14 @@ namespace Pairing
             finally
             {
                 _uList.Remove(node);
-                if
-                    (toRet != null)
-                    Monitor.Exit(node);
+                if (_uList.Any())
+                {
+                    Monitor.Enter(_uList.First);
+                    Monitor.Pulse(_uList.First);
+                    Monitor.Exit(_uList.First);
+                }
                 Monitor.Exit(_monitor);
             }
-
         }
     }
 }
--- Pairing/Program.cs
+++ Pairing/Program.cs
@@ -14,26 +14,27 @@ namespace Pairing
         {
 
             
-
+            
             Thread firsThread = new Thread( ()=> Console.WriteLine(pairing.Provide(5, 999999999)));
             Thread second = new Thread( ()=> Console.WriteLine(pairing.Provide("cinco", 999999999)));
-
+            /*
             Thread third = new Thread(() => Console.WriteLine(pairing.Provide(6, 999999999)));
             Thread fourth = new Thread(() => Console.WriteLine(pairing.Provide("seis", 999999999)));
 
             Thread fifth = new Thread(() => Console.WriteLine(pairing.Provide(7, 999999999)));
             Thread sixth = new Thread(() => Console.WriteLine(pairing.Provide("sete", 999999999)));
 
-
+    */
             second.Start();
             firsThread.Start();
+            /*
             third.Start();
             fourth.Start();
             fifth.Start();
             sixth.Start();
-
+            */
             Console.ReadKey();
-
+           
         }
 
         public static void func()
