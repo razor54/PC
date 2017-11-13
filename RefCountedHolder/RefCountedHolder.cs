293738e0aafe46c1f0c684d0f90fdf2fe0f36a6d
razor54 <andregaudencio5@gmail.com>
2017-11-13 21:51:09 +0000

    added exercise 2

---
+++ RefCountedHolder/RefCountedHolder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RefCountedHolder
+{
+    public class RefCountedHolder<T> where T : class
+    {
+        private T _value;
+        private int _refCount;
+
+        public RefCountedHolder(T v)
+        {
+            _value = v;
+            _refCount = 1;
+        }
+
+        public void AddRef()
+        {
+            Interlocked.MemoryBarrier();
+            if (_refCount == 0)
+                throw new InvalidOperationException();
+            Interlocked.Increment(ref _refCount);
+        }
+
+        public void ReleaseRef()
+        {
+            Interlocked.MemoryBarrier();
+            if (_refCount == 0)
+                throw new InvalidOperationException();
+
+            if (Interlocked.Decrement(ref _refCount) != 0) return;
+
+            IDisposable disposable = _value as IDisposable;
+            _value = null;
+            disposable?.Dispose();
+        }
+
+        public T Value
+        {
+            get
+            {
+                Interlocked.MemoryBarrier();
+                if (_refCount == 0)
+                    throw new InvalidOperationException();
+                return _value;
+            }
+        }
+    }
+}
---
+++ RefCountedHolder/RefCounterHolderTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace RefCountedHolder
+{
+    class RefCounterHolderTest
+    {
+        [Test]
+        public void test1()
+        {
+            string s = "ola";
+            int UPPER = 20;
+            RefCountedHolder<string> refCountedHolder = new RefCountedHolder<string>(s);
+
+            for (int i = 0; i < UPPER; i++)
+            {
+               
+
+                Thread t1 = new Thread(t => { refCountedHolder.AddRef(); });
+                Thread t2 = new Thread(t => { refCountedHolder.AddRef(); });
+
+                Thread t3 = new Thread(t =>
+                {
+                    refCountedHolder.ReleaseRef();
+                    refCountedHolder.ReleaseRef();
+                });
+
+                t1.Start();
+                t2.Start();
+
+                t1.Join();
+                t2.Join();
+
+                t3.Start();
+
+                t3.Join();
+
+                Assert.AreEqual(refCountedHolder.Value, s);
+                
+            }
+            Assert.AreEqual(s,refCountedHolder.Value);
+            refCountedHolder.ReleaseRef();
+
+            Assert.Throws<InvalidOperationException>(()=>
+            {
+                var value = refCountedHolder.Value;      
+            });
+
+
+
+        }
+    }
+}
